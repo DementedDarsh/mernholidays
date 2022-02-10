c7a628c10d2eaae6609ed50badba9e86f99e94a4
DementedDarsh <darsh.chong@agilelab.sg>
2022-02-10 11:39:03 +0800

    sas

---
+++ controllers/holidaysController.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Holiday = require("../models/holidaysModel");
+const router = express.Router();
+
+// GET Seed holidays
+
+// GET All Holidays
+
+// POST Create New Holiday
+
+// GET Individual Holiday
+
+// PUT Edit holiday
+
+// DELETE delete holiday
+
+module.exports = router;
